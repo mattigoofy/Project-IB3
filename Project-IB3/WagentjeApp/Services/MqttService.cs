1f215cbf76bd374ae778fba2ac01b60011aa8d6b
jtommeleyn <jelle-tommeleyn@hotmail.com>
2024-12-19 23:07:50 +0100

    wat opmaak aanpassignen

--- Project-IB3/WagentjeApp/Services/MqttService.cs
+++ Project-IB3/WagentjeApp/Services/MqttService.cs
@@ -37,8 +37,8 @@ namespace WagentjeApp.Services
         private TaskCompletionSource<bool> _executeTrajectCompletionSource;
         private TaskCompletionSource<Measurement> _measurementCompletionSource;
         private User _currentUser; // Voor het opslaan van de huidige gebruiker
-        //private string _mqttServerIp = "192.168.0.143"; // Standaard IP-adres
-        private string _mqttServerIp = "172.18.230.3"; // Standaard IP-adres
+        private string _mqttServerIp = "192.168.0.143"; // Standaard IP-adres
+        //private string _mqttServerIp = "172.18.230.3"; // Standaard IP-adres
 
 
         // Private constructor to prevent instantiation
